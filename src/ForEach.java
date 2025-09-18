d878e8b7fb7a584c6a0cdb4a545792a3e5c26b03
unqxl <whymeservices@internet.ru>
2025-09-18 11:27:55 +0300

    new modules

---
+++ src/ForEach.java
@@ -0,0 +1,26 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] names = {
+                "Helen",
+                "Max",
+                "John",
+                "Alex"
+        };
+
+//        for (int i = 0; i < names.length; i++) {
+//            System.out.println(names[i]);
+//        }
+
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+
+        int[] numbers = new int[100];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 100;
+        }
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
